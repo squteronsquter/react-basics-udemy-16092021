9bb6d1b659b8082867131656435f77c4434e62c2
squteronsquter <squter@squter.pl>
2021-09-16 12:26:57 +0200

    Header component added

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1>Time now is: {Date.now()}</h1>
+    </>
+  )
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Header from './components/header'
 
 const App = () => {
   return (
     <>
+      <Header />
       <h1>Hello</h1>
       <h2>World</h2>
     </>
